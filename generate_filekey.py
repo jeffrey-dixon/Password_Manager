214b7207e96e6c32c65810bb2e6f8717bd16d16b
jeffrey-dixon <71093708+jeffrey-dixon@users.noreply.github.com>
2021-04-27 21:46:53 -0700

    Add files via upload

---
+++ generate_filekey.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from cryptography.fernet import Fernet
+
+# key generation
+key = Fernet.generate_key()
+  
+# string the key in a file
+with open('filekey.key', 'wb') as filekey:
+   filekey.write(key)
