6b04db32749fccb34e80c392f21a4ed9abcc6b30
jeffrey-dixon <71093708+jeffrey-dixon@users.noreply.github.com>
2021-04-27 21:49:37 -0700

    Update generate_filekey.py

--- generate_filekey.py
+++ generate_filekey.py
@@ -1,8 +1,10 @@
 from cryptography.fernet import Fernet
 
-# key generation
-key = Fernet.generate_key()
-  
-# string the key in a file
-with open('filekey.key', 'wb') as filekey:
-   filekey.write(key)
+def key_gen():
+  # key generation
+  key = Fernet.generate_key()
+
+  # string the key in a file
+  with open('filekey.key', 'wb') as filekey:
+     filekey.write(key)
+key_gen()
