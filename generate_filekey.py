24e2857ff6c92b29e874c75b4ffac0251c763905
jeffrey-dixon <71093708+jeffrey-dixon@users.noreply.github.com>
2021-04-27 21:50:30 -0700

    Update generate_filekey.py

--- generate_filekey.py
+++ generate_filekey.py
@@ -1,10 +1,8 @@
 from cryptography.fernet import Fernet
 
+#Creates a symmetric key
 def key_gen():
-  # key generation
   key = Fernet.generate_key()
-
-  # string the key in a file
   with open('filekey.key', 'wb') as filekey:
      filekey.write(key)
 key_gen()
